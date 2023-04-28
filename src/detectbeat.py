3696ad6831fb427815ce73534f0b3251a1ebe20e
finnhittson <hittson.finn@gmail.com>
2023-04-28 11:26:44 -0400

    compile all beat detection hardware into one function

--- src/detectbeat.py
+++ src/detectbeat.py
@@ -1,21 +1,30 @@
 import OSS as oss
 import BPD as bpd
+import accumulate as acc
 import argparse
 import scipy
+import numpy as np
 
 def run_beat_detection(filepath, framesize, hop):
 	# (1) OSS
 	frames = oss.overlap(filepath=filepath, framesize=framesize, hop=hop)
 	log_power, fft_frames = oss.low_power_spectrum(frames)
-	flux = oss.get_flux(log_power, fft_frames)
-	filtered_signal = oss.low_pass_filter(flux, n=len(flux))
+	flux = oss.comp_flux(log_power=log_power)
+	filtered_signal = oss.low_pass_filter(flux)
 
 	# (2) BPD
 	oss_frames = bpd.overlap(data=flux, framesize=2048, hop=hop)
-	Am = bpd.generalized_autocorrelation(oss_frames=oss_frames, c=0.5)
+	Am = bpd.generalized_autocorrelation(frames=oss_frames, c=0.5)
 	#EAC = enhance_harmonics(A=Am)
-	indices, values = bpd.pick_peaks(A=EAC, hop=128)
-	Lm = bpd.evaluate_pulse_train(indices, oss_frames[0])
+	indices, values = bpd.pick_peaks(A=Am)
+	Lm = bpd.evaluate_pulse_train(peaks=indices[0], frame=oss_frames[0])
+
+	# (3) Accumulation
+	Lms = acc.eval_entire_signal(indices, values)
+	y = acc.accumulate_gauss(Lms=Lms)
+	idxs, vals = bpd.find_local_maximums(np.concatenate((np.zeros(98), y[98:])))
+
+	return 344.5*60/idxs[-1]
 
 
 if __name__ == "__main__":
@@ -38,4 +47,5 @@ if __name__ == "__main__":
 
 	parser.set_defaults(framesize=1024, hop=128)
 	args = parser.parse_args()
-	run_beat_detection(args.path, args.framesize, args.hop)
+	beat_estimate = run_beat_detection(args.path, args.framesize, args.hop)
+	print(beat_estimate)
